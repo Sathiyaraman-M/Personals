77e2367efc8307dd6411eeda82629bf2ec10a900
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2024-12-14 21:55:19 +0530

    Rename and update Migration 05 in Infrastructure

---
+++ src/Personals.Infrastructure/Migrations/Migration_05_UpdateLookupTypesTable.cs
@@ -0,0 +1,33 @@
+using FluentMigrator;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Personals.Infrastructure.Migrations;
+
+[SuppressMessage(
+    category: "Naming",
+    checkId: "CA1707:Identifiers should not contain underscores",
+    Justification = "This is a migration file.",
+    Scope = "namespace",
+    Target = "PayFlow.Core.Migrations")]
+[Migration(5)]
+public class Migration_05_UpdateLookupTypesTable : Migration
+{
+    public override void Up()
+    {
+        Delete.FromTable("LookupTypes").AllRows();
+        Alter.Table("LookupTypes").AddColumn("UserId").AsGuid().NotNullable();
+        Create.ForeignKey("FK_LookupTypes_UserId")
+            .FromTable("LookupTypes").ForeignColumn("UserId")
+            .ToTable("AppUsers").PrimaryColumn("Id");
+        Delete.Index("IX_LookupTypes_Code").OnTable("LookupTypes");
+        Create.UniqueConstraint("CodeAndUserId").OnTable("LookupTypes").Columns("Code", "UserId");
+    }
+
+    [ExcludeFromCodeCoverage]
+    public override void Down()
+    {
+        Delete.UniqueConstraint("CodeAndUserId").FromTable("LookupTypes");
+        Delete.Column("UserId").FromTable("LookupTypes");
+        Create.Index("IX_LookupTypes_Code").OnTable("LookupTypes").OnColumn("Code").Unique();
+    }
+}
